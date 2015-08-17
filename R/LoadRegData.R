186ec2a6601569d2a0ed107ea82c2b3afe65d3d6
areedv <biorakel@gmail.com>
2015-08-17 12:40:02 +0200

    add function for providing registry data

---
+++ R/LoadRegData.R
@@ -0,0 +1,18 @@
+#' Provider of data for registries at Rapporteket
+#'
+#' Generic to registries, provide reporting data obtained from sql databases
+#' Underlying this function is rapbase::RapporteketDbConnection
+#' 
+#' @param registryName String Name of the registry as defined in dbConfig.yml
+#' @param query String SQL query to obtain the data
+#' @return RegData dataframe Registry data
+
+LoadRegData <- function(registryName, query, dbType = "mysql") {
+  
+  dbList <- rapbase::rapOpenDbConnection(registryName, dbType)
+  RegData <- DBI::dbGetQuery(dbList$con, query)
+  rapbase::rapCloseConnection(dbList$con, dbList$drv)
+  dbList <- NULL
+  
+  return(RegData)
+}
