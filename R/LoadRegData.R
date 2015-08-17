9db3c7fc17c986ce11c592014daab1badc6503b0
areedv <biorakel@gmail.com>
2015-08-17 15:04:30 +0200

    improve docs

--- R/LoadRegData.R
+++ R/LoadRegData.R
@@ -5,6 +5,7 @@
 #' 
 #' @param registryName String Name of the registry as defined in dbConfig.yml
 #' @param query String SQL query to obtain the data
+#' @param dbType String Type of db to query, currently "mysql" (default) and "mssql"
 #' @return RegData dataframe Registry data
 #' @export
 
