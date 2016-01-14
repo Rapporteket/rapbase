4f4360d327c771233bac7205c074c8c470c15c67
kevinthon <kevin.thon@gmail.com>
2016-01-14 08:36:00 +0100

    Småendringer

--- R/Helligdager2008til2022.R
+++ R/Helligdager2008til2022.R
@@ -1,7 +1,7 @@
 #' Offisielle norske helligdager 2008 til 2022
 #' 
 #' Et datasett med norske røddager 2008 til 2022. Søndager 
-#' er ikke inkludert i listen. \o fdg æ r ø å
+#' er ikke inkludert i listen.
 #'
 #' @format En data frame med 195 rader og 2 kolonner
 #' \describe{
