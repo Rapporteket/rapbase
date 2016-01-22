d9dc801e94f731ee2a5da32574d2b0fde8eb5d56
areedv <biorakel@gmail.com>
2016-01-22 10:18:07 +0100

    first commit simple test tool

---
+++ R/HalloRapporteket.R
@@ -0,0 +1,13 @@
+#' Plain testing tool
+#' 
+#' To be used for testing purposes
+#' 
+#' @return message
+
+HalloRapporteket <- function() {
+  
+  message <- "Hallaisen Rapporteket"
+  
+  return(message)
+  
+}
