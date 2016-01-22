e3b73a909d584d84dfad892e75a16264fd9bf5f7
areedv <biorakel@gmail.com>
2016-01-22 10:14:35 +0100

    recommit???

---
+++ R/LoadRegData.R
@@ -0,0 +1,20 @@
+#' Provider of data for registries at Rapporteket
+#'
+#' Generic to registries, provide reporting data obtained from sql databases
+#' Underlying this function is rapbase::RapporteketDbConnection
+#' 
+#' @param registryName String Name of the registry as defined in dbConfig.yml
+#' @param query String SQL query to obtain the data
+#' @param dbType String Type of db to query, currently "mysql" (default) and "mssql"
+#' @return RegData dataframe Registry data
+#' @export
+
+LoadRegData <- function(registryName, query, dbType = "mysql") {
+  
+  dbList <- rapOpenDbConnection(registryName, dbType)
+  RegData <- DBI::dbGetQuery(dbList$con, query)
+  rapCloseDbConnection(dbList$con)
+  dbList <- NULL
+  
+  return(RegData)
+}
