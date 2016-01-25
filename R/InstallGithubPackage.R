4e1d069e95b4bb31b907905c250f6766d09b9cf5
areedv <biorakel@gmail.com>
2016-01-25 13:46:31 +0100

    initial commit

---
+++ R/InstallGithubPackage.R
@@ -0,0 +1,48 @@
+#' Install packages from GitHub at Rapporteket
+#' 
+#' Install and report results
+#' 
+#' @param packageName String Name of the package
+#' @param branchName String Name of the branch to use
+#' 
+#' @return message String containing logged entries from the function
+#' @export
+
+installGithubPackage <- function(packageName, branchName) {
+  
+  message <- ""
+  message <- MakeMessage(message, "Initiating 'InstallGithubPackage'")
+  
+  HTTP_PROXY <- "http://www-proxy.helsenord.no:8080"
+  USE_PROXY_URL <- "172.29.3.227"
+  USE_PROXY_PORT <- "8080"
+  GITHUB_ORGANIZATION <- "Rapporteket"
+  
+  githubPackage <- paste0(GITHUB_ORGANIZATION, "/", packageName)
+  githubRapbase <- paste0(GITHUB_ORGANIZATION, "/rapbase")
+  
+  message <- MakeMessage(message, "Setting network proxies")
+  Sys.setenv(http_proxy=HTTP_PROXY)
+  httr::set_config(httr::use_proxy(url=USE_PROXY_URL,
+                                   port=as.numeric(USE_PROXY_PORT)))
+
+  message <- MakeMessage(message,
+                         paste0("Intalling 'rapbase' from branch '",
+                                branchName, "'"))
+  devtools::install_github(githubRapbase, ref=branchName, args=c("--clean"))
+  message <- MakeMessage(message, "Done with 'rapbase'")
+  
+  if (packageName != "rapbase") {
+    message <- MakeMessage(message, paste0("Installing '", packageName,
+                                           "' from branch '", branchName, "'"))
+    devtools::install_github(githubPackage, ref=branchName)
+    
+  }
+  else {
+    message <- MakeMessage(message, "No additional packages to install")
+  }
+  
+  message <- MakeMessage(message, "Done")
+  
+  return(message)
+}
---
+++ R/MakeMessage.R
@@ -0,0 +1,33 @@
+#' Provide simple formatting of a sequential message
+#' 
+#' Wrap strings into a message for simple presentation
+#' 
+#' @param story String Text that the message is to be added to
+#' @param message String Message to add
+#' @param newLine Logical [TRUE] Start message at a new line. TRUE by default
+#' @param newPara Logical [FALSE] Start message at new paragraph. Default FALSE
+#' @param timeStamp Logical [TRUE] Add a time stamp to the message. TRUE by
+#' default
+#' @return story String The whole story...
+#' @export
+
+
+MakeMessage <- function(story, message, newLine=TRUE, newPara=FALSE,
+                        timeStamp=TRUE) {
+  
+  if (timeStamp) {
+    message <- paste(format(Sys.time(), "%F %H:%M:%S"), message)
+  }
+  
+  if (newLine & !newPara) {
+    message <- paste0("\n", message)
+  }
+  
+  if (newPara) {
+    message <- paste0("\n\n", message)
+  }
+  
+  story <- paste(story, message)
+  
+  return(story)
+}
