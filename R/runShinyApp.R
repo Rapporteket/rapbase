8efd7741b7ed5c65ed4d7fa8b991506f9f4442e1
areedv <biorakel@gmail.com>
2016-05-04 12:14:34 +0200

    some (stolen) code as well

--- R/runShinyApp.R
+++ R/runShinyApp.R
@@ -4,6 +4,10 @@
 #' attempt is to keep thin function generic (across packages). It might not
 #' prove wise and future implementation might be per-package.
 #' 
+#' Idea and code (partly) taken from Dean Attali's R Blog
+#' (http://www.r-bloggers.com/supplementing-your-r-package-with-a-shiny-app/)
+#' Gratitude.
+#' 
 #' @param appName String providing the name of the shiny application
 #' @param appsDirectoryName String providing the within-package-path to its
 #' shiny application(s). Assumingly, in the source tree for a given R package
@@ -14,5 +18,27 @@
 
 runShinyApp <- function(appName, appsDirectoryName, packageName) {
   
-  
+    # locate all shiny apps
+    validApps <- list.files(system.file(appsDirectoryName,
+                                        package = packageName))
+    
+    validAppsMsg <-
+      paste0(
+        "Valid apps are: '",
+        paste(validApps, collapse = "', '"),
+        "'")
+    
+    # if an invalid app is given, throw an error
+    if (missing(appName) || !nzchar(appName) ||
+        !appName %in% validApps) {
+      stop(
+        'Please run `runShinyApp()` with a valid app name as an argument.n',
+        validExamplesMsg,
+        call. = FALSE)
+    }
+    
+    # find and launch the app
+    appDir <- system.file(appsDirectoryName, appName, package = packageName)
+    shiny::runApp(appDir, display.mode = "normal")
+  }
 }
