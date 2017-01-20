d7eec83d33b689d025643d90bab4022395ba9298
areedv <biorakel@gmail.com>
2017-01-20 13:27:11 +0100

    error in test, haha

--- tests/testthat/test-message.R
+++ tests/testthat/test-message.R
@@ -6,6 +6,6 @@ message <- "New part of story."
 test_that("Function return one element char vector", {
   testMessage <- function() MakeMessage(story = story, message = message)
   expect_equal(length(testMessage), 1)
-  expect_equal(is.atomic(testMessage), TRUE)
-  expect_equal(is.character(testMessage), TRUE)
+  expect_equal(is.atomic(testMessage)), TRUE)
+  expect_equal(is.character(testMessage)), TRUE)
 })
