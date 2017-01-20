f2ae167fb0e9ea8cca6ff1ee0d65f248a293a668
areedv <biorakel@gmail.com>
2017-01-20 13:35:44 +0100

    working example

--- tests/testthat/test-message.R
+++ tests/testthat/test-message.R
@@ -1,11 +1,10 @@
 context("Messages")
 
-story <- "Whole story so far."
-message <- "New part of story."
-
-test_that("Function return one element char vector", {
-  testMessage <- function() MakeMessage(story = story, message = message)
-  expect_equal(length(testMessage), 1)
-  expect_equal(is.atomic(testMessage)), TRUE)
-  expect_equal(is.character(testMessage)), TRUE)
+test_that("Function return one element atomic char vector", {
+  story <- "Whole story so far."
+  message <- "New part of story."
+  
+  expect_equal(length(MakeMessage(story = story, message = message)), 1)
+  expect_true(is.atomic(MakeMessage(story = story, message = message)))
+  expect_true(is.character(MakeMessage(story = story, message = message)))
 })
