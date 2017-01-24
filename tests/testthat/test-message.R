0fe566cf58d33f9807508ccb765f2bf60bb859cf
areedv <biorakel@gmail.com>
2017-01-24 14:15:03 +0100

    more relevant tests..?

--- tests/testthat/test-message.R
+++ tests/testthat/test-message.R
@@ -3,8 +3,9 @@ context("Messages")
 test_that("Function return one element atomic char vector", {
   story <- "Whole story so far."
   message <- "New part of story."
+  newPara <- TRUE
   
-  expect_equal(length(MakeMessage(story = story, message = message)), 1)
-  expect_true(is.atomic(MakeMessage(story = story, message = message)))
-  expect_true(is.character(MakeMessage(story = story, message = message)))
+  expect_equal(length(MakeMessage(story, message, newPara)), 1)
+  expect_true(is.atomic(MakeMessage(story, message, newPara)))
+  expect_true(is.character(MakeMessage(story, message, newPara)))
 })
