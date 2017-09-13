8ab6fea7ab04d95660a2170e0ae1324a51126b84
areedv <biorakel@gmail.com>
2017-09-13 14:14:52 +0200

    one more stupid test...

--- tests/testthat/test-message.R
+++ tests/testthat/test-message.R
@@ -1,11 +1,13 @@
-context("Messages")
+context("Prompting")
 
 test_that("Function return one element atomic char vector", {
   story <- "Whole story so far."
-  message <- "New part of story."
+  event <- "New part of story."
   newPara <- TRUE
   
-  expect_equal(length(MakeMessage(story, message, newPara)), 1)
-  expect_true(is.atomic(MakeMessage(story, message, newPara)))
-  expect_true(is.character(MakeMessage(story, message, newPara)))
+  expect_equal(length(MakeMessage(story, event, newPara)), 1)
+  expect_true(is.atomic(MakeMessage(story, event, newPara)))
+  expect_true(is.character(MakeMessage(story, event, newPara)))
+  
+  expect_message(HalloRapporteket())
 })
