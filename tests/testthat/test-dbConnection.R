85eaf30d8f3398f89400b2eee66096af3bce2053
areedv <biorakel@gmail.com>
2017-10-16 09:46:20 +0200

    testing of db

--- tests/testthat/test-dbConnection.R
+++ tests/testthat/test-dbConnection.R
@@ -3,3 +3,19 @@ context("Handling db connections")
 test_that("Error provided when key has no corresponding config", {
   expect_error(rapOpenDbConnection(registryName = "aNoneExistingRegistryKey"))
 })
+
+test_that("A mysql db connection and driver can be provided and cleaned", {
+  l <- rapOpenDbConnection("rapbase")
+  expect_output(str(l), "List of 2")
+  expect_is(l[[1]], "MySQLConnection")
+  expect_is(l[[2]], "MySQLDriver")
+  expect_true(RMySQL::isIdCurrent(l$con))
+  rapCloseDbConnection(l$con)
+  expect_error(RMySQL::isIdCurrent(l$con))
+})
+
+test_that("Data can be queried from (MySQL) db", {
+  query <- "SELECT * FROM RapbaseTestTable"
+  expect_output(str(LoadRegData("rapbase", query, dbType = "mysql")),
+                "data.frame")
+})
