13fc13f711ab5f8d585ff35a36ac7186dbcd60cb
areedv <biorakel@gmail.com>
2017-10-23 14:16:57 +0200

    preparing new tests

--- tests/testthat/test-installGithubPackage.R
+++ tests/testthat/test-installGithubPackage.R
@@ -3,3 +3,7 @@ context("Github interaction")
 test_that("How does code coverage work...", {
   expect_error(installGithubPackage())
 })
+
+test_that("'rapbase' or any other package can be instlled from github", {
+  
+})
