586e430ab0b4318a1460f07ff8b16e2fda90ee0a
areedv <biorakel@gmail.com>
2017-10-24 14:57:33 +0200

    more testing

--- tests/testthat/test-installGithubPackage.R
+++ tests/testthat/test-installGithubPackage.R
@@ -4,6 +4,10 @@ test_that("How does code coverage work...", {
   expect_error(installGithubPackage())
 })
 
-test_that("'rapbase' or any other package can be instlled from github", {
-  
+test_that("Test package 'nordummy' can be installed from github", {
+  expect_message(installGithubPackage("nordumm", "rel"))
+})
+
+test_that("'rapbase' can be instlled from github", {
+  expect_message(installGithubPackage("rapbase", "rel"))
 })
