5084b7479a40bbf892a1a3adcbac7cbe842b7e52
areedv <biorakel@gmail.com>
2017-10-26 13:43:36 +0200

    error testing

--- tests/testthat/test-installGithubPackage.R
+++ tests/testthat/test-installGithubPackage.R
@@ -8,6 +8,18 @@ test_tnat("Error is provided when no config present", {
   expect_error(installGithubPackage("norummy", "rel", TRUE))
 })
 
+test_that("Error is provided when none existing branch in 'rapbase'", {
+  expect_error(installGithubPackage("rapbase", "noneExistingBranch"))
+})
+
+test_that("Error provided when none existing branch for existing package", {
+  expect_error(installGithubPackage("nordummy", "noneExistingBranch"))
+})
+
+test_that("Error provided wheb noen existing package", {
+  expect_error(installGithubPackage("noneExistingPackage", "rel"))
+})
+
 test_that("Test package 'nordummy' can be installed from github", {
   expect_message(installGithubPackage("nordumm", "rel"))
 })
