15cfda1c0233618c12aba0a23e047d9d0f47a6c0
areedv <biorakel@gmail.com>
2017-10-26 13:32:53 +0200

    test error handling

--- tests/testthat/test-installGithubPackage.R
+++ tests/testthat/test-installGithubPackage.R
@@ -4,6 +4,10 @@ test_that("How does code coverage work...", {
   expect_error(installGithubPackage())
 })
 
+test_tnat("Error is provided when no config present", {
+  expect_error(installGithubPackage("norummy", "rel", TRUE))
+})
+
 test_that("Test package 'nordummy' can be installed from github", {
   expect_message(installGithubPackage("nordumm", "rel"))
 })
