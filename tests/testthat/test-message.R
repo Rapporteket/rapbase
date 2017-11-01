aa9069da037581908bc53cf695b24f221c306769
areedv <biorakel@gmail.com>
2017-11-01 13:44:27 +0100

    redundant but stil no test...

--- tests/testthat/test-message.R
+++ tests/testthat/test-message.R
@@ -7,8 +7,9 @@ test_that("Function return one element atomic char vector", {
   expect_equal(length(MakeMessage(story, event)), 1)
   expect_true(is.atomic(MakeMessage(story, event)))
   expect_true(is.character(MakeMessage(story, event, newPara = TRUE)))
+  expect_message(HalloRapporteket(), "Hallaisen")
 })
 
 test_that("Test function is operational", {
-  expect_message(HalloRapporteket())
+  expect_message(HalloRapporteket(), "Hallaisen")
 })
