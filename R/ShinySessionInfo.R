8b0b7487cf351cd6bcf25bff30ebd097043bda15
areedv <biorakel@gmail.com>
2018-06-01 12:13:59 +0200

    with docs and corrections

--- R/ShinySessionInfo.R
+++ R/ShinySessionInfo.R
@@ -1,12 +1,36 @@
+#' Provide elements from a shiny session object
+#' 
+#' Extracts elements from a shiny session object relevant for user data. This
+#' function will normally be used via its helper functions.
+#'
+#' @param shinySession List shiny session object
+#' @param entity String defining the element to return. One of 'user',
+#' 'groups', 'resh_id' or 'role'
+#' @param testCase Logical defining to use shinySessionInfo on a test case.
+#' For test cases user privileges are provided by a query defined in the
+#' url of the shiny application request. Default FALSE
+#'
+#' @return String of singel user data element
+#' 
+#' @seealso \code{\link{getShinyUserName}}, \code{\link{getShinyUserGroups}},
+#' \code{\link{getShinyUserReshId}}, \code{\link{getShinyUserRole}}
+#' 
+#' @export
+
 shinySessionInfo <- function(shinySession, entity, testCase = FALSE) {
   
-  if (is.null(shinySession)) {
-    stop("No session information provided. Cannot do anything")
+  if (is.null(shinySession) | !is.list(shinySession)) {
+    stop("No list of session information provided. Cannot do anything")
+  }
+  
+  if (!(entity %in% c("user", "groups", "resh_id", "role"))) {
+    stop("Incorrect entity provided. Must be one of 'user', 'groups', 'resh_id'
+         or 'role'")
   }
   
   if (testCase) {
-    warning("This is a test. Not to be applied in production!",
-            immediate. = TRUE)
+    #warning("This is a test. Not to be applied in production!",
+    #        immediate. = TRUE)
     us <- shinySession$clientData$url_search
     user <- us$`X-USER`
     groups <- us$`X-GROUPS`
@@ -15,8 +39,8 @@ shinySessionInfo <- function(shinySession, entity, testCase = FALSE) {
   } else {
     user <- shinySession$user
     groups <- shinySession$groups
-    resh_id <- shinySession$HTTP_RESH_ID
-    role <- shinySession$HTTP_ROLE
+    resh_id <- shinySession$request$HTTP_RESH_ID
+    role <- shinySession$request$HTTP_ROLE
   }
   
   switch(entity,
