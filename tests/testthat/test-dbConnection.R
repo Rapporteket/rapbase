decf576b32cdf6902a973f0db2451d5f78405c85
Are Edvardsen <are.edvardsen@helse-nord.no>
2019-03-11 10:44:16 +0100

    make db for testing in DEV context

--- tests/testthat/test-dbConnection.R
+++ tests/testthat/test-dbConnection.R
@@ -13,6 +13,26 @@ test_that("Error provided when key has no corresponding config", {
   expect_error(rapOpenDbConnection(registryName = "aNoneExistingRegistryKey"))
 })
 
+# make sure we do have a test db during DEV
+if (Sys.getenv("R_RAP_INSTANCE") == "DEV") {
+  query <- c("DROP DATABASE IF EXISTS rapbase;",
+             "CREATE DATABASE rapbase;",
+             "USE rapbase;",
+             paste("CREATE TABLE testTable (id int, someText varchar(50),",
+                   "someInt INT, someBigInt BIGINT, someFloat DOUBLE,",
+                   "someTime DATETIME);"))
+  conf <- getConfig()[["rapbase"]]
+  drv <- RMariaDB::MariaDB()
+  con <- DBI::dbConnect(drv,
+                        host = conf$host,
+                        user = conf$user,
+                        password = conf$pass)
+  for (q in query) {
+    tmp <- DBI::dbExecute(con, q)
+  }
+  DBI::dbDisconnect(con)
+}
+
 test_that("A mysql db connection and driver can be provided and cleaned", {
   l <- rapOpenDbConnection("rapbase")
   expect_output(str(l), "List of 2")
