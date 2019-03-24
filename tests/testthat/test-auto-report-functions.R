eab4ca8dbdb10e2efcad293f5dd73dda1d889a75
Are Edvardsen <are.edvardsen@helse-nord.no>
2019-03-24 15:10:06 +0100

    more testing

--- tests/testthat/test-auto-report-functions.R
+++ tests/testthat/test-auto-report-functions.R
@@ -2,14 +2,16 @@ context("Auto report")
 
 # store current instance
 currentInstance <- Sys.getenv("R_RAP_INSTANCE")
+currentConfigPath <- Sys.getenv("R_RAP_CONFIG_PATH")
 Sys.setenv(R_RAP_INSTANCE="")
+Sys.setenv(R_RAP_CONFIG_PATH="")
 
 synopsis <- "Test of auto report"
 package <- "rapbase"
 fun <- ".testAutoReport"
 paramNames <- c("aNum", "aChar", "anExp")
 paramValues <- c(1, "someString", "Sys.Date()")
-owner <- "Tester"
+owner <- "tester"
 email <- "tester@skde.no"
 runDayOfYear <- as.POSIXlt(Sys.Date())$yday+1
 dryRun <- FALSE
@@ -47,7 +49,33 @@ test_that("Function for testing automated reports return a file", {
 })
 
 test_that("Auto reports can be processed (shipment by email not tested)", {
-  expect_message(runAutoReport(dryRun = TRUE), "No emails sent. Attachment is", all = FALSE)
+  expect_message(runAutoReport(dryRun = TRUE), "No emails sent. Attachment is",
+                 all = FALSE)
+})
+
+test_that("A year-day sequence can be mande", {
+  rdoy <- makeRunDayOfYearSequence(interval = "month")
+  expect_gte(length(rdoy), 10)
+  expect_true(is.numeric(rdoy))
+})
+
+test_that("The next run day in sequence can be identified", {
+  expect_equal(as.numeric(
+    findNextRunDate(runDayOfYear = c(10,20,30), baseDayNum = 11,
+                    returnFormat = "%j")), 20
+    )
+})
+
+shinySession <- list(user="tester")
+shinySession$groups <- "rapbase"
+# shinySession$request <- list(HTTP_RESH_ID="789012")
+# shinySession$request$HTTP_ROLE <- "LC"
+# # make a copy for testing wrong class
+# shinySessionWrongClass <- shinySession
+# simulate ShinySession class for above list
+attr(shinySession, "class") <- "ShinySession"
+test_that("A per-user subscription table (for shiny) can be made", {
+  expect_true(is.list(makeUserSubscriptionTab(session = shinySession)))
 })
 
 reportId <- names(rd)[length(rd)]
@@ -58,5 +86,6 @@ test_that("Auto report can be deleted", {
 })
 
 
-# Restore instance
+# Restore environment
 Sys.setenv(R_RAP_INSTANCE=currentInstance)
+Sys.setenv(R_RAP_CONFIG_PATH=currentConfigPath)
