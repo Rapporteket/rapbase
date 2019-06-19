58f3d7465974c294f5e32009e5cddf10faa5fe0d
Are Edvardsen <are.edvardsen@helse-nord.no>
2019-06-19 14:27:18 +0200

    init commit wrapper functions for full name and phone from session object

---
+++ R/GetUserFullName.R
@@ -0,0 +1,33 @@
+#' Get user full name from config or session object
+#' 
+#' This is a helper function for \code{\link{userInfo}}. When used without a
+#' shiny session object calls to this function is made without any arguments. If
+#' redefining contexts is needed, please use \code{\link{userInfo}} instead.
+#' 
+#' @inheritParams userInfo
+#' 
+#' @return String full name
+#' 
+#' @seealso \code{\link{getUserName}},
+#' \code{\link{getUserGroups}}, \code{\link{getUserReshId}},
+#' \code{\link{getUserEmail}}, \code{\link{getUserPhone}}
+#' 
+#' @examples
+#' \dontrun{
+#' getUserFullName()
+#' getUserFullName(shinySessionObject)
+#' }
+#' 
+#' @export
+
+
+getUserFullName <- function(shinySession = NULL) {
+  
+  if (missing(shinySession)) {
+    warning(paste("A shinySession object was not provided. Hence, this",
+                  "function call may not work across all contexts"))
+  }
+  
+  userInfo(shinySession, entity = "full_name")
+  
+}
