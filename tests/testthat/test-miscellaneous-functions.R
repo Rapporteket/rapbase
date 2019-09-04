8a8d2cdaa571f9202353f6d34f198674a6fb9acd
Are Edvardsen <biorakel@gmail.com>
2019-09-04 13:52:59 +0200

    better test coverage

--- tests/testthat/test-miscellaneous-functions.R
+++ tests/testthat/test-miscellaneous-functions.R
@@ -1,11 +1,16 @@
 context("Miscellaneos functions")
 
+
 test_that("Listing of Rapporteket packages is present", {
   expect_true(is.vector(getRapPackages()))
 })
 
 test_that("Widget for shiny apps can be provided", {
   expect_output(str(appNavbarUserWidget()), "script|navbar-brand")
+})
+
+test_that("widget for shiny apps can be provided with user info added", {
+  expect_output(str(appNavbarUserWidget(addUserInfo = TRUE)), "script|navbar-brand")
 })
 
 # currently, testing of scheduler function is not at all sufficient...
@@ -20,6 +25,13 @@ file.copy(system.file(c("rapbaseConfig.yml", "dbConfig.yml"),
 test_that("scheduler kick-off function can be run based on default config (that should provide warnings)", {
   expect_warning(fireInTheHole())
 })
+
+
+# produce a pop-up text...
+test_that("an html doc regarding 'how we deal with...' can be provided", {
+  expect_output(howWeDealWithPersonalData(session = list()), "")
+})
+
 
 # Restore env
 Sys.setenv(R_RAP_INSTANCE=currentContext)
