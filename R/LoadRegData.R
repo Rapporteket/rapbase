19e64fd7f330beac632eb132497db31b19a0b0ba
Are Edvardsen <biorakel@gmail.com>
2021-03-10 15:06:42 +0100

    new function for db metadata

--- R/LoadRegData.R
+++ R/LoadRegData.R
@@ -7,9 +7,12 @@
 #' @param query String SQL query to obtain the data
 #' @param dbType String Type of db to query, currently "mysql" (default) and
 #' "mssql"
-#' @return regData data frame containing registry data
+#' @param tabs Character vector for optional definition of tables to describe.
+#' Defaults to an empty vector in which case all tables are used
+#' @return data frame containing registry data or a list with table names and
+#' corresponding fields with attributes
 #' @name loadRegData
-#' @aliases LoadRegData loadRegData
+#' @aliases LoadRegData loadRegData describeRegistryDb
 NULL
 
 #' @rdname loadRegData
@@ -46,4 +49,26 @@ loadRegData <- function(registryName, query, dbType = "mysql") {
   dbList <- NULL
 
   return(regData)
+}
+
+#' @rdname loadRegData
+#' @export
+describeRegistryDb <- function(registryName, tabs = c()) {
+
+  qGetTabs <- "SHOW TABLES;"
+  qGetDesc <- "DESCRIBE "
+
+  desc <- list()
+
+  if (length(tabs) == 0) {
+    tabs <- rapbase::loadRegData(registryName = registryName,
+                                 query = qGetTabs)[[1]]
+  }
+
+  for (tab in tabs) {
+    query <- paste0(qGetDesc, tab, ";")
+    desc[[tab]] <- loadRegData(registryName, query)
+  }
+
+  desc
 }
