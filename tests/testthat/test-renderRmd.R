31e27e9b3b007694067ef494fd2499ff3e8cd12e
Are Edvardsen <biorakel@gmail.com>
2021-07-15 14:30:35 +0200

    unit tests

---
+++ tests/testthat/test-renderRmd.R
@@ -0,0 +1,10 @@
+test_that("Rmd source can be rendered", {
+	sourceFile <- system.file("testReportSource.Rmd", package = "rapbase")
+	logoFile <- system.file("template/logo.png", package = "rapbase")
+  expect_true(file.exists(renderRmd(sourceFile)))
+  expect_true(file.exists(renderRmd(sourceFile, outputType = "pdf",
+  																	logoFile = logoFile,
+  																	params = list(reglogo = "logo"))))
+  expect_error(renderRmd(sourceFile, outputType = "beamer"))
+  expect_error(renderRmd(sourceFile = "noneExistingFile.Rmd"))
+})
