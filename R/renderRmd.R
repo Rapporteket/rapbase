425dcaf4705477ea2588c7d84205d564dc08f873
Are Edvardsen <biorakel@gmail.com>
2021-07-16 10:15:57 +0200

    provide html fragments also

--- R/renderRmd.R
+++ R/renderRmd.R
@@ -2,16 +2,17 @@
 #'
 #' Function that renders documents at Rapporteket from rmarkdown sorce files.
 #' Output formats may be (vanilla) HTML or PDF based on our own pandoc latex
-#' template. Allow for parameters to be part of report processing. However,
+#' template or fragements of html when the result is to be embedded in existing
+#' web pages. Rmarkdown allow parameters to be part of report processing. Thus,
 #' parameters that are speciffic to reports must be provided (as a named list)
-#' when calling this function.
+#' when calling \code{renderRmd()}.
 #'
 #' @param sourceFile Character string providing the path to the rmarkdown
 #' source file.
-#' @param outputType Character string giving the output format. Must be either
-#' "pdf" or "html" (default).
-#' @param logoFile Character string with path to the logo to be used.
-#' Often, this will be the registry logo. Only png and pdf graphics are
+#' @param outputType Character string specifying the output format. Must be one
+#' of \code{c("pdf", "html", "html_fragment")}. Default value is "html".
+#' @param logoFile Character string with path to the logo to be used for PDF
+#' output. Often, this will be the registry logo. Only PNG and PDF graphics are
 #' allowed. Default value is \code{NULL} in which case no such logo will be
 #' added to the output document.
 #' @param params List of report parameters (as named values) to override the
@@ -19,16 +20,18 @@
 #' header. Default is \code{NULL} in which case no parameters as defined in the
 #' rmarkdown document will be overridden.
 #'
-#' @return Character string with path to the rendered file. The file is named
-#' according to \code{tempfile()} with an empty pattern and with the
-#' extention according to \code{outputType} ("pdf" or "html")
+#' @return Character string with path to the rendered file or, if
+#' \code{outputType} is set to "html_fragment", a character string providing an
+#' html fragment. Files are named according to \code{tempfile()} with an empty
+#' pattern and with the extention according to \code{outputType}
+#' ("pdf" or "html").
 #' @export
 
 renderRmd <- function(sourceFile, outputType = "html", logoFile = NULL,
 											params = list()) {
 
 	stopifnot(file.exists(sourceFile))
-	stopifnot(outputType %in% c("html", "pdf"))
+	stopifnot(outputType %in% c("html", "html_fragment", "pdf"))
 
 	# do work in tempdir and return to origin on exit
 	owd <- setwd(tempdir())
@@ -45,20 +48,26 @@ renderRmd <- function(sourceFile, outputType = "html", logoFile = NULL,
 		file.copy(logoFile, ".", overwrite = TRUE)
 	}
 
-	rmarkdown::render(
+	f <- rmarkdown::render(
 		input = basename(sourceFile),
 		output_format =
 			switch(
 				outputType,
 				pdf = bookdown::pdf_document2(pandoc_args = c("--template=default.latex")),
 				html = bookdown::html_document2(),
+				html_fragment = bookdown::html_fragment2(),
 				beamer = rmarkdown::beamer_presentation(theme = "Hannover"),
 				reveal = revealjs::revealjs_presentation(theme = "sky")
 			),
-		#intermediates_dir = tempdir(),
 		output_file = tempfile(pattern = ""),
 		clean = TRUE,
 		params = params,
 		envir = new.env())
+
+	if (outputType == "html_fragment") {
+		return(shiny::HTML(readLines(f)))
+	} else {
+		return(f)
+	}
 
 }
--- tests/testthat/test-renderRmd.R
+++ tests/testthat/test-renderRmd.R
@@ -2,6 +2,8 @@ test_that("Rmd source can be rendered", {
 	sourceFile <- system.file("testReportSource.Rmd", package = "rapbase")
 	logoFile <- system.file("template/logo.png", package = "rapbase")
   expect_true(file.exists(renderRmd(sourceFile)))
+  expect_true("html" %in%
+  							class(renderRmd(sourceFile, outputType = "html_fragment")))
   expect_true(file.exists(renderRmd(sourceFile, outputType = "pdf",
   																	logoFile = logoFile,
   																	params = list(reglogo = "logo"))))
