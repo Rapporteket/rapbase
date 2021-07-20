ffd3763935185b55d83986c192c2f2d82074316e
Are Edvardsen <biorakel@gmail.com>
2021-07-20 09:51:52 +0200

    better docs

--- R/makeStandardTable.R
+++ R/makeStandardTable.R
@@ -7,27 +7,31 @@
 #' \code{prettyTab()} creates a table with cells containing percentages of
 #' totals and observations optinally with a marginal row count
 #'
-#' @param tab Data frame or matrix represetnting the table
+#' @param tab Data frame or matrix represetnting the table.
 #' @param col_names Character vector with column names. Defaults
-#' \code{colnames(tab)}
+#' \code{colnames(tab)}.
 #' @param type Character string defining output, either "html" or "pdf".
 #' Default is "pdf".
-#' @param cap Character string with table caption. Empty string by default
+#' @param cap Character string with table caption. Empty string by default.
 #' @param label Character string defining the label in case the table needs to
 #' be referenced elsewhere in the overall document. For instance, setting this
 #' to 'my_table' the corresponding inline rmarkdown reference to use is
 #' \code{\\@ref(tab:my_table)}. Please note that for this to work for both
 #' LaTex and HTML the bookdown document processing functions must be used,
-#' \emph{i.e.} bookdown:pdf_document2() and bookdown::html_document2(),
-#' respectively. Default value is \code{knitr::opts_current$get("label")}.
-#' @param digs Numeric number of digits to use. 0 by default
+#' \emph{i.e.} \code{bookdown::pdf_document2()} and
+#' \code{bookdown::html_document2()}, respectively. Default value is
+#' \code{knitr::opts_current$get("label")} in which case the name of the
+#' current R chunk will be used as label.
+#' @param digs Integer number of digits to use. 0 by default.
 #' @param align Character vector specifying column alignment in the LaTeX way,
 #' \emph{e.g.} \code{c("l", "c", "r")} will align the first column to the left,
-#' center the second and right-aling the last one. Default is NULL in which case
-#' @param fs Numeric providing the font size. Only apply for LaTeX output.
-#' Default value is 8
-#' @param lsd Logical if table is to be scaled down. Only apply for LaTeX
-#' output. FALSE by default
+#' center the second and right-aling the last one. Default is NULL in which
+#' case numeric columns are right-aligned and all other columns are
+#' left-aligned.
+#' @param fs Integer providing the font size. Applies only for pdf output.
+#' Default value is 8.
+#' @param lsd Logical if table is to be scaled down. Applies only for pdf
+#' output. FALSE by default.
 #'
 #' @return Character string containing RMarkdown table code
 #' @name makeStandardTable
