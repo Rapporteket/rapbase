ef0a028b5f756dfe5c456d35e9a9841422329e0a
Are Edvardsen <biorakel@gmail.com>
2021-07-21 12:57:08 +0200

    test now part of ci

--- tests/testthat/test-siteReferenceInSync.R
+++
@@ -1,6 +0,0 @@
-test_that("all functions in pkg are listed _pkgdown.yml reference", {
-  expect_warning(
-    pkgdown::build_reference(pkg = find.package("rapbase"), preview = FALSE),
-    regexp = NA
-  )
-})
