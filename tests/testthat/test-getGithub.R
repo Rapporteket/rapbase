52030814db0d94bc1be5245fb41170eed8efce79
Are Edvardsen <biorakel@gmail.com>
2021-07-26 13:59:55 +0200

    very simple github api using gh

---
+++ tests/testthat/test-getGithub.R
@@ -0,0 +1,11 @@
+test_that("error when dont't know what", {
+  expect_error(getGithub("harrypotter", "wand"))
+})
+
+test_that("contributors are provided", {
+  expect_equal(class(getGithub("contributors", "rapbase")), "character")
+})
+
+test_that("key can be provided", {
+  expect_true(grepl("ssh-rsa", getGithub("keys", "areedv")))
+})
