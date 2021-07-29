c08c5171786584d0c18496de2f0b409dcf4af361
Are Edvardsen <biorakel@gmail.com>
2021-07-29 14:42:13 +0200

    first proper shiny module by rapbase :-)

--- R/export.R
+++ R/export.R
@@ -1,18 +1,152 @@
-#' Tools for exporting data
+#' Shiny modules providing GUI and server logic for Export
 #'
-#' Functions tools for registries that want implement exporting of registry
-#' databases for local development purposes.
+#' Functions for registries that wants to implement exporting of registry
+#' databases, \emph{e.g.} for local development purposes. Also includes
+#' relevant helper functions
 #'
+#' @param id Character string module ID
 #' @param registryName Character string registry name key
 #' @param pubkey Character vector with public keys
 #' @param compress Logical if export data is to be compressed (using gzip).
 #' FALSE by default.
 #' @param session Shiny session object
 #'
-#' @return Some data structure with info obtained from the github api
+#' @return Shiny objects, mostly. Helper functions may return other stuff too.
 #' @name export
-#' @aliases selectListPubkey exportDb
+#' @aliases exportUCInput exportUCServer exportUCApp selectListPubkey exportDb
 NULL
+
+# shiny modules
+#' @rdname export
+#' @export
+exportUCInput <- function(id) {
+  shiny::tagList(
+    shiny::uiOutput(shiny::NS(id, "exportPidUI")),
+    shiny::uiOutput(shiny::NS(id, "exportKeyUI")),
+    shiny::checkboxInput(shiny::NS(id, "exportCompress"), "Komprimer eksport"),
+    shiny::uiOutput(shiny::NS(id, "exportEncryptUI")),
+    shiny::uiOutput(shiny::NS(id, "exportDownloadUI"))
+  )
+}
+
+#' @rdname export
+#' @export
+exportUCServer <- function(id, registryName) {
+  shiny::moduleServer(id, function(input, output, session) {
+
+    rv <- shiny::reactiveValues(
+      exportFile = NULL,
+      exportText = "Kryptér",
+      exportIcon = "lock-open",
+      exportClass = "btn-warning"
+    )
+
+    pubkey <- shiny::reactive({
+      shiny::req(input$exportPid)
+      rapbase::getGithub("keys", input$exportPid)
+    })
+
+    ## observers
+    shiny::observeEvent(input$exportPid, {
+      rv$exportFile <- NULL
+      rv$exportText <- "Kryptér"
+      rv$exportIcon <- "lock-open"
+      rv$exportClass <- "btn-warning"
+    })
+    shiny::observeEvent(input$exportKey, {
+      rv$exportFile <- NULL
+      rv$exportText <- "Kryptér"
+      rv$exportIcon <- "lock-open"
+      rv$exportClass <- "btn-warning"
+    })
+    shiny::observeEvent(input$exportCompress, {
+      rv$exportFile <- NULL
+      rv$exportText <- "Kryptér"
+      rv$exportIcon <- "lock-open"
+      rv$exportClass <- "btn-warning"
+    })
+
+    shiny::observeEvent(input$exportEncrypt, {
+      if (is.null(rv$exportFile)) {
+        f <- rapbase::exportDb(registryName,
+                               compress = input$exportCompress,
+                               session = session)
+        rv$exportFile <- sship::enc(f, pid = NULL, pubkey = input$exportKey)
+        rv$exportText <- "Kryptert!"
+        rv$exportIcon <- "lock"
+        rv$exportClass <- "btn-success"
+      }
+    })
+
+    output$exportDownload <- shiny::downloadHandler(
+      filename = basename(rv$exportFile),
+      content = function(file) {
+        file.copy(rv$exportFile, file)
+        rapbase::repLogger(
+          session,
+          msg = paste("Db export file", basename(rv$exportFile), "downloaded"))
+      }
+    )
+
+    ## UC
+    output$exportPidUI <- shiny::renderUI({
+      shiny::selectInput(
+        shiny::NS(id, "exportPid"),
+        label = shiny::tags$div(
+          shiny::HTML(as.character(shiny::icon("user")), "Velg mottaker:")
+        ),
+        choices =  rapbase::getGithub("contributors", registryName))
+    })
+    output$exportKeyUI <- shiny::renderUI({
+      if (length(pubkey()) == 0) {
+        shiny::p("No keys found!")
+      } else {
+        shiny::selectInput(
+          shiny::NS(id, "exportKey"),
+          label = shiny::tags$div(
+            shiny::HTML(as.character(shiny::icon("key")), "Velg nøkkel:")
+          ),
+          choices = rapbase::selectListPubkey(pubkey()))
+      }
+    })
+    output$exportEncryptUI <- shiny::renderUI({
+      shiny::actionButton(
+        shiny::NS(id, "exportEncrypt"),
+        label = rv$exportText,
+        icon = shiny::icon(rv$exportIcon),
+        class = rv$exportClass)
+    })
+    output$exportDownloadUI <- shiny::renderUI({
+      if (is.null(rv$exportFile)) {
+        NULL
+      } else {
+        shiny::tagList(
+          shiny::hr(),
+          shiny::downloadButton(
+            shiny::NS(id, "exportDownload"),
+            label = "Last ned!")
+        )
+      }
+    })
+
+  })
+}
+
+#' @rdname export
+#' @export
+exportUCApp <- function(registryName = "rapbase") {
+  ui <- shiny::fluidPage(
+    exportUCInput("rapbaseExport")
+  )
+  server <- function(input, output, session) {
+    exportUCServer("rapbaseExport", registryName)
+  }
+
+  shiny::shinyApp(ui, server)
+}
+
+
+# the rest is helper functions
 
 #' @rdname export
 #' @export
@@ -51,3 +185,5 @@ exportDb <- function(registryName, compress = FALSE, session) {
 
   invisible(f)
 }
+
+
