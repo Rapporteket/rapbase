e8defc5ded6ede25b0234cebc5560582256e0176
Are Edvardsen <biorakel@gmail.com>
2021-08-02 14:00:56 +0200

    a working module (new pkg imports)

--- R/stats.R
+++ R/stats.R
@@ -4,16 +4,17 @@
 #' The intended purpose is to provide registry staff access to when and by
 #' whom the resources at Rapporteket were used, \emph{i.e.} application
 #' start-up and single report usage. As such, this will be a tool to provide
-#' useful statistics. However, it might also serve as a formal logging utility
-#' but only if carefully implemented throughout the relevant functions that
-#' make up the registry application at Rapporteket.
+#' useful statistics. However, it might also serve as a formal monitor utility
+#' but only if logging is carefully implemented throughout the relevant
+#' functions that make up the registry application at Rapporteket.
 #'
 #' @param id Character string shiny module id
 #' @param registryName Character string registry name key
 #'
 #' @return Shiny objects, mostly. Helper functions may return other stuff too.
 #' @name stats
-#' @aliases statsInput statsUI statsServer statsApp
+#' @aliases statsInput statsUI statsServer statsApp getRegistryLog logFormat
+#' logTimeFrame
 #'
 #' @examples
 #' # client user interface function
@@ -41,15 +42,15 @@ statsInput <- function(id) {
 
   shiny::tagList(
     shiny::radioButtons(
-      shiny::NS(id, "stats"), "Kategori:",
-      choices = c("Innlogging", "Enkeltrapporter")
+      shiny::NS(id, "type"), "Kategori:",
+      choices = list(Innlogging = "app", Enkeltrapporter = "report")
     ),
     shiny::uiOutput(shiny::NS(id, "period")),
     shiny::radioButtons(
       shiny::NS(id, "downloadFormat"), "Nedlastingsformat:",
-      choices = list(csv = "csv", csvNordic = "csv-nordisk")
+      choices = c("csv", "xlsx-csv")
     ),
-    shiny::uiOutput(shiny::NS(id, "download"))
+    shiny::downloadButton(shiny::NS(id, "download"), "Last ned!")
   )
 }
 
@@ -58,7 +59,7 @@ statsInput <- function(id) {
 statsUI <- function(id) {
 
   shiny::tagList(
-    shiny::uiOutput(shiny::NS(id, "pivot"))
+    rpivotTable::rpivotTableOutput(shiny::NS(id, "pivot"))
   )
 }
 
@@ -69,20 +70,44 @@ statsServer <- function(id, registryName) {
 
   shiny::moduleServer(id, function(input, output, session) {
 
+    log <- shiny::reactive({
+      getRegistryLog(registryName, input$type) %>%
+        logFormat()
+    })
+
+    logFrame <- shiny::reactive({
+      logTimeFrame(log(), input$period[1], input$period[2])
+    })
+
     output$period <- shiny::renderUI({
       shiny::dateRangeInput(
         shiny::NS(id, "period"), "Periode:",
-        start = Sys.Date(), end = Sys.Date(), separator = "-"
+        start = min(log()$date), end = max(log()$date), separator = "-"
       )
     })
-    output$download <- shiny::renderUI({
-      shiny::downloadButton(
-        shiny::NS(id, "download"), "Last ned!"
-      )
-    })
-    output$pivot <- shiny::renderUI({
-      NULL
-    })
+    output$download <- shiny::downloadHandler(
+      filename = function() {
+        basename(
+          tempfile(
+            pattern = paste0("useStats-", registryName),
+            fileext = ".csv"
+          )
+        )
+      },
+      content = function(file) {
+        if (input$downloadFormat == "xlsx-csv") {
+          readr::write_excel_csv2(logFrame(), file)
+        } else {
+          readr::write_csv2(logFrame(), file)
+        }
+      }
+    )
+    output$pivot <- rpivotTable::renderRpivotTable(
+      rpivotTable::rpivotTable(logFrame(),
+                               rows = c("name"),
+                               cols = c("year", "month"),
+                               rendererName = "Heatmap")
+    )
   })
 }
 
@@ -102,4 +127,61 @@ statsApp <- function() {
   }
 
   shiny::shinyApp(ui, server)
+}
+
+
+#' @rdname stats
+#' @export
+getRegistryLog <- function(registryName, type = "app") {
+
+  stopifnot(type == "report" | type == "app")
+  path <- Sys.getenv("R_RAP_CONFIG_PATH")
+
+  if (path == "") {
+    stop(paste("No path to log-files provided. Make sure the environment",
+               "varaible R_RAP_CONFIG_PATH is set!"))
+  }
+
+  logFile <- switch(type,
+                    "report" = file.path(path, "reportLog.csv"),
+                    "app" = file.path(path, "appLog.csv")
+  )
+  if (!file.exists(logFile)) {
+    stop(paste("Cannot find the log!", logFile, "does not exist."))
+  }
+
+  log <- utils::read.csv(logFile,
+                         header = TRUE,
+                         stringsAsFactors = FALSE)
+  invisible(log)
+
+}
+
+#' @rdname stats
+#' @export
+logFormat <- function(log) {
+
+  log <- log %>%
+    dplyr::mutate(
+      datetime = as.POSIXct(time),
+      date = as.Date(datetime),
+      time = format(datetime, "%H:%M:%S"),
+      year = lubridate::year(datetime),
+      month = lubridate::month(datetime),
+      day = lubridate::day(datetime),
+      weekday = lubridate::wday(
+        datetime,
+        week_start = 1,
+        abbr = FALSE)
+    )
+
+  invisible(log)
+}
+
+#' @rdname stats
+#' @export
+logTimeFrame <- function(log, startDate, endDate) {
+
+  dplyr::filter(log, date >= startDate & date <= endDate)
+
 }
