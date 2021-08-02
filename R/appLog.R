ad68e928fbdda0266a319db95a3e0f295f1ea83e
Are Edvardsen <biorakel@gmail.com>
2021-08-02 14:34:11 +0200

    data set for testing

---
+++ R/appLog.R
@@ -0,0 +1,15 @@
+#' App log test dataset.
+#'
+#' A dataset containing test entries for the application log.
+#'
+#' @format A data frame with 20 rows and 7 variables:
+#' \describe{
+#'   \item{time}{character timestamp}
+#'   \item{user}{user name}
+#'   \item{name}{user full name}
+#'   \item{group}{users group/registry}
+#'   \item{role}{users role}
+#'   \item{resh_id}{users organization}
+#'   \item{message}{log message}
+#' }
+"appLog"
