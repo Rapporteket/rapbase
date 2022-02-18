57737a0810e2f30c66b1f0268e402ec9334a277c
Are Edvardsen <biorakel@gmail.com>
2022-02-18 14:48:59 +0100

    clean and delete staging data

--- R/stagingData.R
+++ R/stagingData.R
@@ -2,27 +2,41 @@
 #'
 #' These functions are used to handle staging data at Rapporteket.
 #'
+#' \code{cleanStagingData()} globally removes all staging data files older than
+#' the end-of-life age provided. This is potentially a vastly destructive
+#' function that should be used with great care.
+#'
 #' @param registryName Character string providing the registry name
 #' @param dataName Character string providing the data set name.
 #' @param data A data object such as a data.frame to be stored as
 #' \code{dataName}.
 #' @param dir Character string providing the path to where the staging data
 #' directory resides. Default value is \code{Sys.getenv("R_RAP_CONFIG_PATH")}.
+#' @param eolAge Numeric providing the age in seconds which after staging files
+#' are to be removed
+#' @param dryRun Logical defining if function is to be run in dry (none
+#' destructive) mode
 #'
-#' @return \code{saveStagingData()} returns the data object (\code{data}),
-#' invisibly. \code{loadStagingData()} returns the data object corresponding to
-#' the name given upon saving (\code{dataName}). If the requested data set for
-#' loading does not exist the function returns FALSE. \code{pathStagingData()}
-#' is primarily an internal helper function and returns a character string with
-#' the path to the staging directory of \code{registryName}. If its parent
-#' directory (\code{dir}) does not exists an error is returned.
+#' @return \itemize{
+#'   \item \code{saveStagingData()} returns the data object (\code{data}),
+#'     invisibly.
+#'   \item \code{loadStagingData()} returns the data object corresponding to
+#'     the name given upon saving (\code{dataName}). If the requested data set
+#'     for loading does not exist the function returns FALSE.
+#'   \item \code{deleteStagingData()} returns TRUE if the file was deleted and
+#'     FALSE if not.
+#'   \item \code{cleanStagingData()} returns a list of files (to be) removed.
+#'   \item \code{rapbase:::pathStagingData()} is an internal helper function and
+#'     returns a character string with the path to the staging directory of
+#'     \code{registryName}. If its parent directory (\code{dir}) does not exists
+#'     an error is returned.
+#' }
+#'
 #' @name stagingData
-#' @aliases pathStagingData saveStagingData loadStagingData
+#' @aliases saveStagingData loadStagingData deleteStagingData cleanStagingData
+#' pathStagingData
 #'
 #' @examples
-#' ## Check and make the path to staging data
-#' path <- pathStagingData(registryName = "myReg", dir = tempdir())
-#'
 #' ## Prep test data
 #' registryName <- "myReg"
 #' dataName <- "testData"
@@ -30,29 +44,11 @@
 #' dir <- tempdir()
 #'
 #' ## Save data for staging
-#' sageStagingData(registryName, dataName, data, dir)
+#' saveStagingData(registryName, dataName, data, dir)
 #'
 #' ## Retrieve data set from staging
 #' loadStagingData(registryName, dataName, dir)
 NULL
-
-
-#' @rdname stagingData
-#' @export
-pathStagingData <- function(registryName, dir) {
-
-  stopifnot(dir.exists(dir))
-
-  parentPath <- "stagingData"
-
-  path <- file.path(dir, parentPath, registryName)
-
-  if (!dir.exists(path)) {
-    dir.create(path, recursive = TRUE)
-  }
-
-  path
-}
 
 
 #' @rdname stagingData
@@ -79,4 +75,66 @@ loadStagingData <- function(registryName, dataName,
   } else {
     FALSE
   }
+}
+
+#' @rdname stagingData
+#' @export
+deleteStagingData <- function(registryName, dataName,
+                              dir = Sys.getenv("R_RAP_CONFIG_PATH")) {
+
+  path <- pathStagingData(registryName, dir)
+  filePath <- file.path(path, dataName)
+
+  if (file.exists(filePath)) {
+    file.remove(filePath)
+    TRUE
+  } else {
+    FALSE
+  }
+}
+
+#' @rdname stagingData
+#' @export
+cleanStagingData <- function(eolAge, dryRun = TRUE) {
+
+  if (Sys.getenv("R_RAP_CONFIG_PATH") == "") {
+    stop(paste("Got no path to staging data. No data will be deleted.",
+               "Exiting."))
+  }
+
+  dir <- Sys.getenv("R_RAP_CONFIG_PATH")
+  parentPath <- "stagingData"
+  path <- file.path(dir, parentPath)
+  f <- normalizePath(list.files(path, recursive = TRUE, full.names = TRUE))
+  fAge <- as.numeric(Sys.time()) - as.numeric(file.mtime(f))
+  fDelete <- f[fAge > eolAge]
+
+  if (dryRun) {
+    message(
+      paste("Function invoked in dry run mode and none of the returned files\n",
+            "will be deleted.\n",
+            "To delete the files please re-run this function with the dryRun\n",
+            "argument set to 'TRUE'. Godspeed!")
+    )
+    fDelete
+  } else {
+    file.remove(fDelete)
+    invisible(fDelete)
+  }
+}
+
+#' @rdname stagingData
+pathStagingData <- function(registryName, dir) {
+
+  stopifnot(dir.exists(dir))
+
+  parentPath <- "stagingData"
+
+  path <- file.path(dir, parentPath, registryName)
+
+  if (!dir.exists(path)) {
+    dir.create(path, recursive = TRUE)
+  }
+
+  path
 }
--- tests/testthat/test-stagingData.R
+++ tests/testthat/test-stagingData.R
@@ -1,9 +1,14 @@
+# store current instance
+currentConfigPath <- Sys.getenv("R_RAP_CONFIG_PATH")
+
 # test data
 registryName <- "testReg"
-dir <- tempdir()
+Sys.setenv(R_RAP_CONFIG_PATH = tempdir())
 dataName <- "testData"
 d <- mtcars
-testPath <- file.path(dir, "stagingData", registryName)
+testPath <- file.path(Sys.getenv("R_RAP_CONFIG_PATH"),
+                      "stagingData",
+                      registryName)
 testFile <- file.path(testPath, dataName)
 
 test_that("staging cannot commence if paret directory does not exist", {
@@ -14,18 +19,48 @@ test_that("staging cannot commence if paret directory does not exist", {
 })
 
 test_that("relevant directories are created at first time use", {
-  expect_equal(pathStagingData(registryName, dir), testPath)
+  expect_equal(
+    pathStagingData(registryName,
+                    dir = Sys.getenv("R_RAP_CONFIG_PATH")),
+    testPath)
 })
 
 test_that("data can be stored for staging", {
-  expect_equal(saveStagingData(registryName, dataName, d, dir), d)
+  expect_equal(saveStagingData(registryName, dataName, d), d)
   expect_true(file.exists(testFile))
 })
 
 test_that("data can be retrieved from staging", {
-  expect_equal(loadStagingData(registryName, dataName, dir), d)
+  expect_equal(loadStagingData(registryName, dataName), d)
 })
 
 test_that("loading none-existing data returns false", {
-  expect_false(loadStagingData(registryName, "imaginaryDataSet", dir))
+  expect_false(loadStagingData(registryName, "imaginaryDataSet"))
 })
+
+test_that("deleting a none-existing file returns FALE", {
+  expect_false(deleteStagingData(registryName, "imaginaryDataSet"))
+})
+
+test_that("a file can be deleted", {
+  expect_true(deleteStagingData(registryName, dataName))
+  expect_false(file.exists(testFile))
+})
+
+test_that("a global clean of staging data can be performed (also dry run)", {
+  expect_equal(saveStagingData(registryName, dataName, d), d)
+  expect_true(file.exists(testFile))
+  expect_message(cleanStagingData(0))
+  expect_equal(class(cleanStagingData(0)), "character")
+  expect_true(file.exists(testFile))
+  expect_invisible(cleanStagingData(0, dryRun = FALSE))
+  expect_false(file.exists(testFile))
+})
+
+test_that("a global clean of staging data will stop if no parent directory", {
+  Sys.unsetenv("R_RAP_CONFIG_PATH")
+  expect_error(cleanStagingData(0))
+})
+
+# Restore environment
+Sys.setenv(R_RAP_CONFIG_PATH = currentConfigPath)
