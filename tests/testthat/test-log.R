556912d30791c9ba12e354c7e7eb1a5a471ce8ad
Are Edvardsen <biorakel@gmail.com>
2022-03-31 13:47:20 +0200

    debug ci

--- tests/testthat/test-log.R
+++ tests/testthat/test-log.R
@@ -164,7 +164,7 @@ appEvent <- data.frame(
 
 test_that("app event can be appended to db", {
   check_db()
-  expect_silent(appendLog(event = appEvent, name = "appLog"))
+  expect_warning(appendLog(event = appEvent, name = "appLog"))
 })
 
 test_that("append errors when target is not known", {
