239429a4f5cdd5e2ce24be9c06a823baa064bfa7
Are Edvardsen <biorakel@gmail.com>
2022-05-06 11:31:03 +0200

    file merge stat modules

--- R/stats.R
+++ R/stats.R
@@ -174,6 +174,66 @@ statsApp <- function() {
 }
 
 
+#' Shiny modules providing the Stats Guide
+#'
+#' @param id Character string module ID
+#' @param registryName Character string registry name key
+#'
+#' @return Functions ui and server representing the (module) app
+#' @name statsGuide
+#' @aliases statsGuideUI statsGuideServer statsGuideApp
+#' @examples
+#' ui <- shiny::fluidPage(
+#'   statsGuideUI("statsGuide")
+#' )
+#'
+#' server <- function(input, output, session) {
+#'   statsGuideServer("statsGuide", "test")
+#' }
+#'
+#' if (interactive()) {
+#'   shiny::shinyApp(ui, server)
+#' }
+NULL
+
+#' @rdname statsGuide
+#' @export
+statsGuideUI <- function(id) {
+
+  shiny::htmlOutput(shiny::NS(id, "statsGuide"))
+}
+
+#' @rdname statsGuide
+#' @export
+statsGuideServer <- function(id, registryName) {
+
+  shiny::moduleServer(id, function(input, output, session) {
+
+    output$statsGuide <- shiny::renderUI({
+      rapbase::renderRmd(
+        sourceFile = system.file("statsGuide.Rmd", package = "rapbase"),
+        outputType = "html_fragment",
+        params = list(registryName = registryName))
+    })
+  })
+}
+
+#' @rdname statsGuide
+#' @export
+statsGuideApp <- function() {
+
+  ui <- shiny::fluidPage(
+    statsGuideUI("statsGuide")
+  )
+
+  server <- function(input, output, session) {
+    statsGuideServer("statsGuide", "test")
+  }
+
+  shiny::shinyApp(ui, server)
+}
+
+
 #' @rdname stats
 #' @export
 logFormat <- function(log) {
