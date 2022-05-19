8df4328927a9b51ff9383fae8973fea9769977b0
Are Edvardsen <biorakel@gmail.com>
2022-05-19 09:06:06 +0200

    temporary, revert at sship v0.8 #124

--- R/moduleExport.R
+++ R/moduleExport.R
@@ -67,7 +67,7 @@ exportUCServer <- function(id, registryName, repoName = registryName,
     pubkey <- shiny::reactive({
       shiny::req(input$exportPid)
       keys <- rapbase::getGithub("keys", input$exportPid)
-      sship::pubkey_filter(keys, "rsa")
+      pubkey_filter(keys, "rsa")
     })
 
     encFile <- shiny::reactive({
@@ -250,4 +250,35 @@ exportDb <- function(registryName, compress = FALSE, session) {
   rapbase::repLogger(session, msg = paste(registryName, "Db dump created."))
 
   invisible(f)
+}
+
+
+#' Filter ssh public keys by type
+#'
+#' From a vector of ssh public keys, return those that are of a given type.
+#'
+#' @param keys Vector of strings representing ssh public keys.
+#' @param type Character string defining the ssh public key type that will pass
+#' the filter. Relevant values are strings returned by
+#' \code{attributes(openssl::read_pubkey(pubkey))$class[2]}, \emph{e.g.} "rsa"
+#' and "dsa".
+#'
+#'
+#' @return A vector of strings representing (filtered) public keys.
+#' @export
+#'
+#' @examples
+#' ## make ssh public key strings
+#' rsa_pubkey <- openssl::write_ssh(openssl::rsa_keygen()$pubkey)
+#' dsa_pubkey <- openssl::write_ssh(openssl::dsa_keygen()$pubkey)
+#'
+#' ## filter keys by type
+#' pubkey <- pubkey_filter(c(rsa_pubkey, dsa_pubkey), "rsa")
+#' identical(pubkey, rsa_pubkey)
+
+pubkey_filter <- function(keys, type) {
+
+  pass <- function(x, y) attributes(openssl::read_pubkey(x))$class[2] == y
+
+  keys[vapply(keys, pass, logical(length = 1), y = type)]
 }
