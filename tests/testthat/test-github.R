b1dab7b0d09e9e38b0103e8a5712a34112325ca6
areedv <biorakel@gmail.com>
2023-06-09 14:01:57 +0200

    mock gh api calls

--- tests/testthat/test-github.R
+++ tests/testthat/test-github.R
@@ -1,11 +1,13 @@
-test_that("error when dont't know what", {
-  expect_error(getGithub("harrypotter", "wand"))
-})
+with_mock_dir("gh_api_response", {
+  test_that("error when dont't know what", {
+    expect_error(getGithub("harrypotter", "wand"))
+  })
 
-test_that("contributors are provided", {
-  expect_equal(class(getGithub("contributors", "rapbase")), "character")
-})
+  test_that("contributors are provided", {
+    expect_equal(class(getGithub("contributors", "rapbase")), "character")
+  })
 
-test_that("key can be provided", {
-  expect_true(grepl("ssh-rsa", getGithub("keys", "areedv")))
+  test_that("key can be provided", {
+    expect_true(grepl("ssh-rsa", getGithub("keys", "areedv")))
+  })
 })
