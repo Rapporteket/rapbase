8e80c680d77013328a67410c636c76d081662b14
arnfinn <arnfinn.steindal@gmail.com>
2024-06-04 12:52:35 +0000

    polish

--- tests/testthat/test-renderRmd.R
+++ tests/testthat/test-renderRmd.R
@@ -17,12 +17,8 @@ test_that("Rmd source can be rendered", {
   logoFile <- system.file("template/logo.png", package = "rapbase")
   expect_true(file.exists(renderRmd(sourceFile)))
   expect_true("html" %in%
-    class(renderRmd(sourceFile, outputType = "html_fragment")))
-#  expect_true(file.exists(renderRmd(sourceFile,
-#    outputType = "pdf",
-#    logoFile = logoFile,
-#    params = list(reglogo = "logo")
-#  )))
+      class(renderRmd(sourceFile,outputType = "html_fragment"))
+  )
   expect_error(renderRmd(sourceFile, outputType = "beamer"))
   expect_error(renderRmd(sourceFile = "noneExistingFile.Rmd"))
 })
