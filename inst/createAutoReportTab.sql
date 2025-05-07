4164b2179b1086ef39e5a7af22267598f4f938e0
Arnfinn Hykkerud Steindal <arnfinn.steindal@gmail.com>
2025-05-07 04:28:45 -0400

    Autoreport-kode: kun for database (#244)
    Dropper alt som har med autoreport og lesing fra yaml-fil. Kun støtte
    for autoreport-oppføringer i database

--- inst/createAutoReportTab.sql
+++ inst/createAutoReportTab.sql
@@ -1,18 +1,19 @@
-DROP TABLE IF EXISTS `autoreportTest`;
-CREATE TABLE `autoreportTest` (
-  `id` varchar(32) COLLATE utf8mb3_danish_ci DEFAULT NULL,
-  `synopsis` varchar(74) COLLATE utf8mb3_danish_ci DEFAULT NULL,
-  `package` varchar(17) COLLATE utf8mb3_danish_ci DEFAULT NULL,
-  `fun` varchar(28) COLLATE utf8mb3_danish_ci DEFAULT NULL,
-  `params` text COLLATE utf8mb3_danish_ci,
-  `owner` varchar(14) COLLATE utf8mb3_danish_ci DEFAULT NULL,
-  `email` varchar(48) COLLATE utf8mb3_danish_ci DEFAULT NULL,
-  `organization` varchar(22) COLLATE utf8mb3_danish_ci DEFAULT NULL,
-  `terminateDate` varchar(10) COLLATE utf8mb3_danish_ci DEFAULT NULL,
-  `interval` varchar(7) COLLATE utf8mb3_danish_ci DEFAULT NULL,
-  `intervalName` varchar(11) COLLATE utf8mb3_danish_ci DEFAULT NULL,
-  `runDayOfYear` text COLLATE utf8mb3_danish_ci,
-  `type` varchar(12) COLLATE utf8mb3_danish_ci DEFAULT NULL,
-  `ownerName` varchar(26) COLLATE utf8mb3_danish_ci DEFAULT NULL,
-  `startDate` varchar(10) COLLATE utf8mb3_danish_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_danish_ci;
+/* Database: Autoreport */
+DROP TABLE IF EXISTS `autoreport`;
+CREATE TABLE `autoreport` (
+  `id` varchar(255) COLLATE utf8mb4_danish_ci DEFAULT NULL,
+  `synopsis` varchar(255) COLLATE utf8mb4_danish_ci DEFAULT NULL,
+  `package` varchar(255) COLLATE utf8mb4_danish_ci DEFAULT NULL,
+  `fun` varchar(255) COLLATE utf8mb4_danish_ci DEFAULT NULL,
+  `params` text COLLATE utf8mb4_danish_ci,
+  `owner` varchar(255) COLLATE utf8mb4_danish_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8mb4_danish_ci DEFAULT NULL,
+  `organization` varchar(255) COLLATE utf8mb4_danish_ci DEFAULT NULL,
+  `terminateDate` varchar(255) COLLATE utf8mb4_danish_ci DEFAULT NULL,
+  `interval` varchar(255) COLLATE utf8mb4_danish_ci DEFAULT NULL,
+  `intervalName` varchar(255) COLLATE utf8mb4_danish_ci DEFAULT NULL,
+  `runDayOfYear` text COLLATE utf8mb4_danish_ci,
+  `type` varchar(255) COLLATE utf8mb4_danish_ci DEFAULT NULL,
+  `ownerName` varchar(255) COLLATE utf8mb4_danish_ci DEFAULT NULL,
+  `startDate` varchar(255) COLLATE utf8mb4_danish_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_danish_ci;
--- tests/testthat/test-auto-report-db.R
+++ tests/testthat/test-auto-report-db.R
@@ -1,6 +1,7 @@
 # store current instance and prepare
 currentInstance <- Sys.getenv("R_RAP_INSTANCE")
 currentConfig <- Sys.getenv("R_RAP_CONFIG_PATH")
+currentDb <- Sys.getenv("MYSQL_DB_AUTOREPORT")
 
 Sys.setenv(R_RAP_CONFIG_PATH = tempdir())
 
@@ -10,7 +11,7 @@ file.copy(
 )
 configFile <- file.path(Sys.getenv("R_RAP_CONFIG_PATH"), "rapbaseConfig.yml")
 
-nameAutoReportDb <- "autoreportTest"
+nameAutoReportDb <- "autoreporttest"
 
 # get some auto report data to work on, i.e. default rapbase
 arSample <- yaml::read_yaml(system.file("autoReport.yml", package = "rapbase"))
@@ -24,7 +25,8 @@ yaml::write_yaml(config, configFile)
 # adjust config and get whatever name of logging database define there
 config$r$autoReport$target <- "db"
 yaml::write_yaml(config, configFile)
-nameAutoReportDb <- "autoreportTest"
+nameAutoReportDb <- "autoreporttest"
+Sys.setenv(MYSQL_DB_AUTOREPORT = nameAutoReportDb)
 
 test_that("env vars needed for testing is present", {
   check_db()
@@ -68,15 +70,15 @@ test_that("table can be created in auto report db", {
 
 test_that("a sample of auto report data can be written to db", {
   check_db()
-  expect_null(writeAutoReportData(config = arSample, target = "db"))
+  expect_null(writeAutoReportData(config = arSample))
 })
 
 test_that("sample auto report data can be read from db", {
   check_db()
-  expect_equal(nrow(readAutoReportData(target = "db")), 7)
-  expect_equal(class(readAutoReportData(target = "db")), "data.frame")
-  writeAutoReportData(config = arSample, target = "db")
-  expect_equal(nrow(readAutoReportData(target = "db")), 14)
+  expect_equal(nrow(readAutoReportData()), 7)
+  expect_equal(class(readAutoReportData()), "data.frame")
+  writeAutoReportData(config = arSample)
+  expect_equal(nrow(readAutoReportData()), 14)
 })
 
 # For a valid test make sure there is ONE standard dummy report scheduled
@@ -87,8 +89,7 @@ test_that("Auto reports can be processed monthly", {
   check_db()
   expect_message(runAutoReport(
     dato = "2024-12-01",
-    dryRun = TRUE,
-    target = "db"
+    dryRun = TRUE
     ),
     "No emails sent. Content is:",
     all = FALSE
@@ -101,8 +102,7 @@ test_that("Auto reports can be processed weekly", {
   check_db()
   expect_message(runAutoReport(
     dato = "2024-12-02",
-    dryRun = TRUE,
-    target = "db"
+    dryRun = TRUE
     ),
     "No emails sent. Content is:",
     all = FALSE
@@ -116,8 +116,7 @@ test_that("Bulletin reports can be processed (monthly)", {
     runAutoReport(
       dato = "2024-12-01",
       type = c("bulletin"),
-      dryRun = TRUE,
-      target = "db"
+      dryRun = TRUE
       ),
     "No emails sent. Content is: This is a simple",
     all = FALSE
@@ -128,8 +127,7 @@ test_that("Auto reports not sent because of no reports this date", {
   check_db()
   expect_silent(runAutoReport(
     dato = "2024-12-03",
-    dryRun = TRUE,
-    target = "db"
+    dryRun = TRUE
     ))
 })
 
@@ -137,8 +135,7 @@ test_that("Auto reports not sent if before start date", {
   check_db()
   expect_silent(runAutoReport(
     dato = "1800-01-01",
-    dryRun = TRUE,
-    target = "db"
+    dryRun = TRUE
     ))
 })
 
@@ -146,8 +143,7 @@ test_that("Auto reports not sent if after start date", {
   check_db()
   expect_silent(runAutoReport(
     dato = "3000-01-01",
-    dryRun = TRUE,
-    target = "db"
+    dryRun = TRUE
     ))
 })
 
@@ -162,7 +158,7 @@ if (is.null(check_db(is_test_that = FALSE))) {
       "FALK_APP_ID" = "80"
     ),
     code = {
-      registryName <- "autoreportTest"
+      registryName <- "autoreporttest"
       ## make a list for report metadata
       reports <- list(
         FirstReport = list(
@@ -272,6 +268,233 @@ if (is.null(check_db(is_test_that = FALSE))) {
   )
 }
 
+#########################################
+# Tests from test-auto-report-functions #
+# Spring 2025                           #
+#########################################
+
+# make test data
+synopsis <- "Test of auto report"
+package <- "rapbase"
+type <- "subscription"
+fun <- ".testAutoReport"
+paramNames <- c("aNum", "aChar", "anExp")
+paramValues <- c(1, "someString", "Sys.Date()")
+owner <- "tester"
+ownerName <- "Tore Tester"
+email <- "tester@skde.no"
+organization <- "000000"
+runDayOfYear <- as.POSIXlt(Sys.Date())$yday + 1
+dryRun <- FALSE
+
+test_that("Auto report can be created and written to file", {
+  check_db()
+  expect_silent(createAutoReport(
+    synopsis, package, type, fun, paramNames,
+    paramValues, owner, email, organization,
+    runDayOfYear, dryRun
+  ))
+})
+
+test_that("auto report tables (for shiny) can be made", {
+  shinySession <- list(user = "tester")
+  shinySession$groups <- "rapbase"
+  attr(shinySession, "class") <- "ShinySession"
+  mapOrgId <- data.frame(id = "999999", name = "HUS", stringsAsFactors = FALSE)
+
+  check_db()
+  expect_true(is.list(
+    makeAutoReportTab(shinySession,
+      type = "subscription", mapOrgId = mapOrgId,
+      includeReportId = TRUE
+    )
+  ))
+  expect_true(is.list(
+    makeAutoReportTab(shinySession,
+      type = "dispatchment", mapOrgId = mapOrgId,
+      includeReportId = TRUE
+    )
+  ))
+  expect_true(is.list(
+    makeAutoReportTab(shinySession,
+      type = "bulletin", mapOrgId = mapOrgId,
+      includeReportId = TRUE
+    )
+  ))
+
+  expect_true(is.list(
+    makeAutoReportTab(shinySession, type = "dispatchment", mapOrgId)
+  ))
+
+})
+
+
+test_that("Auto report can be deleted", {
+  check_db()
+  createAutoReport(synopsis, package, type, fun, paramNames,
+                   paramValues, owner, ownerName, email, organization,
+                   runDayOfYear = as.numeric(format(Sys.Date(), "%j")),
+                   startDate = as.character(Sys.Date() + 1)
+  )
+  rd <- readAutoReportData()
+  reportId <- names(rd)[length(rd)]
+  expect_silent(deleteAutoReport(reportId))
+  expect_true(is.na(names(readAutoReportData())[reportId]))
+})
+
+
+
+withr::with_envvar(
+  new = c(
+    "R_RAP_INSTANCE" = "QAC",
+    "FALK_EXTENDED_USER_RIGHTS" = "[{\"A\":80,\"R\":\"LC\",\"U\":1},{\"A\":80,\"R\":\"SC\",\"U\":2},{\"A\":81,\"R\":\"LC\",\"U\":2}]",
+    "FALK_APP_ID" = "80",
+    "SHINYPROXY_USERNAME" = "ttesterc",
+    "SHINYPROXY_USERGROUPS" = "rapbase,rapbase,utils,utils",
+    "USERORGID" = "[1, 2, 3, 4]",
+    "USERFULLNAME" = "Tore Tester Container",
+    "USEREMAIL" = "ttesterc@rapporteket.no"
+  ),
+  code = {
+    registryName <- "rapbase"
+
+    # prep arguments
+    ## make a list for report metadata
+    reports <- list(
+      FirstReport = list(
+        synopsis = "First example report",
+        fun = "fun1",
+        paramNames = c("organization", "outputFormat"),
+        paramValues = c(111111, "html")
+      ),
+      SecondReport = list(
+        synopsis = "Second example report",
+        fun = "fun2",
+        paramNames = c("organization", "outputFormat"),
+        paramValues = c(111111, "pdf")
+      )
+    )
+
+    ## make a list of organization names and numbers
+    orgs <- list(
+      OrgOne = 111111,
+      OrgTwo = 222222
+    )
+
+    ## make a list for report metadata
+    reports <- list(
+      FirstReport = list(
+        synopsis = "First example report",
+        fun = "fun1",
+        paramNames = c("organization", "outputFormat"),
+        paramValues = c(100082, "html")
+      ),
+      SecondReport = list(
+        synopsis = "Second example report",
+        fun = "fun2",
+        paramNames = c("organization", "outputFormat"),
+        paramValues = c(102966, "pdf")
+      )
+    )
+    ## make a list of organization names and numbers
+    orgs <- list(
+      OrgOne = 100082,
+      OrgTwo = 102966
+    )
+    type <- "subscription"
+    user <- userAttribute(unit = 1)
+    for (n in names(user)) {
+      user[[n]] <- shiny::reactiveVal(user[[n]])
+    }
+
+    test_that("module server provides sensible output", {
+      check_db()
+      shiny::testServer(autoReportServer,
+                        args = list(
+                          registryName = registryName, type = type,
+                          reports = reports, orgs = orgs, user = user
+                        ),
+                        {
+                          session$setInputs(report = "FirstReport")
+                          expect_equal(class(output$reports), "list")
+                        }
+      )
+    })
+
+    test_that("no report select list created when no reports available", {
+      check_db()
+      shiny::testServer(
+        autoReportServer,
+        args = list(
+          registryName = registryName, type = type,
+          reports = NULL, orgs = orgs, user = user
+        ),
+        {
+          expect_true(is.null(output$reports))
+        }
+      )
+    })
+
+    type <- "dispatchment"
+    test_that("email can be added and deleted for dispatchment", {
+      check_db()
+      shiny::testServer(
+        autoReportServer,
+        args = list(
+          registryName = registryName, type = type,
+          org = shiny::reactive(100082),
+          reports = reports, orgs = orgs, user = user
+        ),
+        {
+          session$setInputs(email = "true@email.no")
+          expect_equal(length(autoReport$email), 0)
+          session$setInputs(addEmail = 1)
+          expect_equal(autoReport$email[1], "true@email.no")
+          session$setInputs(delEmail = 1)
+          expect_equal(length(autoReport$email), 0)
+        }
+      )
+    })
+
+    test_that("add email button is not created if email is not valid", {
+      check_db()
+      shiny::testServer(
+        autoReportServer,
+        args = list(
+          registryName = registryName, type = type,
+          org = shiny::reactive(100082),
+          reports = reports, orgs = orgs, user = user
+        ),
+        {
+          session$setInputs(email = "invalid@email-format")
+          expect_true(is.null(output$editEmail))
+          session$setInputs(email = "invalid@email-format.o")
+          expect_true(is.null(output$editEmail))
+          session$setInputs(email = "invalid.email-format.on")
+          expect_true(is.null(output$editEmail))
+        }
+      )
+    })
+
+    test_that("no submit button is provided when module is not eligible", {
+      check_db()
+      shiny::testServer(
+        autoReportServer,
+        args = list(
+          registryName = registryName, type = "subscription",
+          reports = reports, orgs = orgs, eligible = shiny::reactiveVal(FALSE), user = user
+        ),
+        {
+          session$setInputs(email = "valid.email@format.no")
+          expect_true(is.null(output$makeAutoReport))
+        }
+      )
+    })
+
+  }
+)
+
+
 
 # remove test db
 if (is.null(check_db(is_test_that = FALSE))) {
@@ -289,3 +512,5 @@ if (is.null(check_db(is_test_that = FALSE))) {
 # Restore instance
 Sys.setenv(R_RAP_CONFIG_PATH = currentConfig)
 Sys.setenv(R_RAP_INSTANCE = currentInstance)
+Sys.setenv(MYSQL_DB_AUTOREPORT = currentDb)
+
