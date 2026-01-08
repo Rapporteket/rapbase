fa3af29f91c2b21ced476a92d9baff6e8bfee34e
Arnfinn Hykkerud Steindal <arnfinn.hykkerud.steindal@helse-nord.no>
2026-01-08 13:59:45 +0100

    doc2

--- R/moduleNavbarWidget.R
+++ R/moduleNavbarWidget.R
@@ -339,6 +339,9 @@ appNavbarUserWidget <- function(user = "Undefined person",
 #'
 #' Render text on how Rapporteket deals with personal data
 #'
+#' @param callerPkg Character string naming the package this function was
+#'   called from. The value is used to display the current version of the
+#'   R package representing the registry at Rapporteket.
 #' @param ... Further arguments, currently not used
 #'
 #' @return fragment html info text
