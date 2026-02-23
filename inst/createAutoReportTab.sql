862f110c2167df4405676909a23a0d481f61af96
Arnfinn Hykkerud Steindal <arnfinn.hykkerud.steindal@helse-nord.no>
2026-02-23 10:48:17 +0100

    Universell sql-kode for å lage tabeller
    Gjelder tabellene appLog, reportLog og autoreport.

--- inst/createAutoReportTab.sql
+++ inst/createAutoReportTab.sql
@@ -1,19 +1,18 @@
 /* Database: Autoreport */
-DROP TABLE IF EXISTS `autoreport`;
-CREATE TABLE `autoreport` (
-  `id` varchar(255) COLLATE utf8mb4_danish_ci DEFAULT NULL,
-  `synopsis` varchar(255) COLLATE utf8mb4_danish_ci DEFAULT NULL,
-  `package` varchar(255) COLLATE utf8mb4_danish_ci DEFAULT NULL,
-  `fun` varchar(255) COLLATE utf8mb4_danish_ci DEFAULT NULL,
-  `params` text COLLATE utf8mb4_danish_ci,
-  `owner` varchar(255) COLLATE utf8mb4_danish_ci DEFAULT NULL,
-  `email` varchar(255) COLLATE utf8mb4_danish_ci DEFAULT NULL,
-  `organization` varchar(255) COLLATE utf8mb4_danish_ci DEFAULT NULL,
-  `terminateDate` varchar(255) COLLATE utf8mb4_danish_ci DEFAULT NULL,
-  `interval` varchar(255) COLLATE utf8mb4_danish_ci DEFAULT NULL,
-  `intervalName` varchar(255) COLLATE utf8mb4_danish_ci DEFAULT NULL,
-  `runDayOfYear` text COLLATE utf8mb4_danish_ci,
-  `type` varchar(255) COLLATE utf8mb4_danish_ci DEFAULT NULL,
-  `ownerName` varchar(255) COLLATE utf8mb4_danish_ci DEFAULT NULL,
-  `startDate` varchar(255) COLLATE utf8mb4_danish_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_danish_ci;
+CREATE TABLE autoreport (
+  id varchar(255) PRIMARY KEY,
+  synopsis varchar(255) DEFAULT NULL,
+  package varchar(255) DEFAULT NULL,
+  fun varchar(255) DEFAULT NULL,
+  params varchar(1025),
+  owner varchar(255) DEFAULT NULL,
+  email varchar(255) DEFAULT NULL,
+  organization varchar(255) DEFAULT NULL,
+  terminateDate varchar(255) DEFAULT NULL,
+  `interval` varchar(255) DEFAULT NULL,
+  intervalName varchar(255) DEFAULT NULL,
+  runDayOfYear varchar(255) DEFAULT NULL,
+  type varchar(255) DEFAULT NULL,
+  ownerName varchar(255) DEFAULT NULL,
+  startDate varchar(255) DEFAULT NULL
+);
