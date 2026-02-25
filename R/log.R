006e14d087f0c6e0cafd74556c3e1beeb5c297d1
Arnfinn Hykkerud Steindal <arnfinn.hykkerud.steindal@helse-nord.no>
2026-02-25 09:23:43 +0100

    Send inn rolle og resh til repLogger
    Closes #327

--- R/log.R
+++ R/log.R
@@ -144,12 +144,20 @@ appLogger <- function(session, msg = "No message provided",
 #' }
 #'
 repLogger <- function(session, msg = "No message provided",
-                      .topcall = sys.call(-1), .topenv = parent.frame()) {
+                      .topcall = sys.call(-1), .topenv = parent.frame(),
+                      role = NULL, resh_id = NULL) {
   name <- "reportLog"
   parent_environment <- environmentName(topenv(.topenv))
   parent_call <- deparse(.topcall, width.cutoff = 160L, nlines = 1L)
+  sessionData <- getSessionData()
+  if (!is.null(role)) {
+    sessionData$role <- role
+  }
+  if (!is.null(resh_id)) {
+    sessionData$resh_id <- resh_id
+  }
   content <- c(
-    getSessionData(),
+    sessionData,
     list(
       environment = parent_environment,
       call = parent_call,
