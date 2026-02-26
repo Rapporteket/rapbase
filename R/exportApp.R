c78b90de851dd7e3b52df8ea21a11f967e1ff996
arnfinn <arnfinn.steindal@gmail.com>
2026-02-26 15:54:06 +0100

    Flytt server og ui ut av exportApp
    Gjør det enklere å teste server-delen.

--- R/exportApp.R
+++ R/exportApp.R
@@ -9,7 +9,18 @@
 #' will be done in JSON format.
 #' @export
 exportApp <- function(teamName = "", dbName = "data", logAsJson = TRUE) {
-  ui <- shiny::navbarPage(
+  if (logAsJson) {
+    loggerSetup()
+  }
+  shiny::shinyApp(ui = exportAppUi, server = exportAppServer)
+}
+
+#' Export app UI
+#'
+#' @returns UI function for exportApp
+#' @keywords internal
+exportAppUi <- function() {
+  shiny::navbarPage(
     id = "navbarpage",
     title = rapbase::title("Simple export app"),
     windowTitle = "Simple export app",
@@ -26,92 +37,86 @@ exportApp <- function(teamName = "", dbName = "data", logAsJson = TRUE) {
       )
     )
   )
-  server <- function(input, output, session) {
-    user <- navbarWidgetServer2(
-      id = "navbar-widget",
-      orgName = "exportApp"
-    )
+}
 
-    shiny::observeEvent(user$role(), {
-      if (user$role() != "SC") {
-        shiny::removeTab(inputId = "navbarpage", target = "exportPanel")
-      } else {
-        shiny::appendTab(
-          "navbarpage",
-          shiny::tabPanel(
-            title = "Eksport",
-            value = "exportPanel",
-            shiny::sidebarLayout(
-              shiny::sidebarPanel(
-                shiny::uiOutput("exportSidebarPanel")
-              ),
-              shiny::mainPanel(
-                shiny::uiOutput("exportMainPanel")
-              )
+#' Export app server
+#'
+#' @param input Shiny input object
+#' @param output Shiny output object
+#' @param session Shiny session object
+#'
+#' @returns Server function for exportApp
+#' @keywords internal
+exportAppServer <- function(input, output, session) {
+  user <- navbarWidgetServer2(
+    id = "navbar-widget",
+    orgName = "exportApp"
+  )
+  shiny::observeEvent(user$role(), {
+    if (user$role() != "SC") {
+      shiny::removeTab(inputId = "navbarpage", target = "exportPanel")
+    } else {
+      shiny::appendTab(
+        "navbarpage",
+        shiny::tabPanel(
+          title = "Eksport",
+          value = "exportPanel",
+          shiny::sidebarLayout(
+            shiny::sidebarPanel(
+              shiny::uiOutput("exportSidebarPanel")
+            ),
+            shiny::mainPanel(
+              shiny::uiOutput("exportMainPanel")
             )
           )
         )
-      }
+      )
     }
-    )
-
-    # User control
-    output$exportSidebarPanel <- shiny::renderUI({
-      if (user$role() == "SC") {
-        exportUCInput("export")
-      } else {
-        return(NULL)
-      }
-    })
-
-    exportUCServer("export", dbName = dbName, teamName = teamName)
-
-    # User guide
-    output$exportMainPanel <- shiny::renderUI({
-      if (user$role() == "SC") {
-        exportGuideUI("exportGuide")
-      } else {
-        return(NULL)
-      }
-    })
-
-    exportGuideServer("exportGuide", dbName)
-
-    ## Metadata
-    meta <- shiny::reactive({
-      describeRegistryDb(registryName = dbName)
-    })
-
-    output$metaControl <- shiny::renderUI({
-      tabs <- names(meta())
-      shiny::selectInput("metaTab", "Velg tabell:", tabs)
-    })
-
-    output$metaDataTable <- DT::renderDataTable(
-      meta()[[input$metaTab]], rownames = FALSE,
-      options = list(lengthMenu = c(25, 50, 100, 200, 400))
-    )
-
-    output$metaData <- shiny::renderUI({
-      DT::dataTableOutput("metaDataTable")
-    })
-
-    output$n_lines <- shiny::renderUI({
-      shiny::h4(paste0(
-        input$metaTab,
-        " har ",
-        nlinesRegistryDb(
-          registryName = dbName,
-          tab = shiny::req(input$metaTab)
-        ),
-        " linjer"
-      )
-      )
-    })
-
   }
-  if (logAsJson) {
-    loggerSetup()
-  }
-  shiny::shinyApp(ui, server)
+  )
+  # User control
+  output$exportSidebarPanel <- shiny::renderUI({
+    if (user$role() == "SC") {
+      exportUCInput("export")
+    } else {
+      return(NULL)
+    }
+  })
+  exportUCServer("export", dbName = dbName, teamName = teamName)
+  # User guide
+  output$exportMainPanel <- shiny::renderUI({
+    if (user$role() == "SC") {
+      exportGuideUI("exportGuide")
+    } else {
+      return(NULL)
+    }
+  })
+  exportGuideServer("exportGuide", dbName)
+  ## Metadata
+  meta <- shiny::reactive({
+    describeRegistryDb(registryName = dbName)
+  })
+  output$metaControl <- shiny::renderUI({
+    tabs <- names(meta())
+    shiny::selectInput("metaTab", "Velg tabell:", tabs)
+  })
+  output$metaDataTable <- DT::renderDataTable(
+    meta()[[input$metaTab]], rownames = FALSE,
+    options = list(lengthMenu = c(25, 50, 100, 200, 400))
+  )
+  output$metaData <- shiny::renderUI({
+    DT::dataTableOutput("metaDataTable")
+  })
+  output$n_lines <- shiny::renderUI({
+    shiny::h4(paste0(
+      input$metaTab,
+      " har ",
+      nlinesRegistryDb(
+        registryName = dbName,
+        tab = shiny::req(input$metaTab)
+      ),
+      " linjer"
+    )
+    )
+  })
 }
