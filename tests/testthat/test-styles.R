90d84bcc0a07fdd32f82b497f44a71da404c58c4
Arnfinn Hykkerud Steindal <arnfinn.hykkerud.steindal@helse-nord.no>
2026-04-16 16:02:12 +0200

    Behold test av opprinnelig funksjon

--- tests/testthat/test-styles.R
+++ tests/testthat/test-styles.R
@@ -29,3 +29,34 @@ test_that("rapTheme function returns a valid shinytheme object", {
     # Check if rubbish theme name returns an error
     expect_error(rapTheme("qwerty"))
 })
+
+# KOPI AV DEN OVER. SLETT NÅR FUNKSJONEN ER FJERNET FRA PAKKEN
+test_that("title function returns a div with the correct structure", {
+    result <- title("Test Title")
+
+    # Check if the result is a shiny tag
+    expect_s3_class(result, "shiny.tag")
+
+    # Check if the div contains the title text
+    expect_true(grepl("Test Title", as.character(result)))
+
+    # Check if the div contains a svg element (the logo)
+    expect_true(grepl("</svg>", as.character(result)))
+})
+
+# KOPI AV DEN OVER. SLETT NÅR FUNKSJONEN ER FJERNET FRA PAKKEN
+test_that("theme function returns a valid shinytheme object", {
+
+    # Check if the theme name is correctly set
+    expect_true(grepl("flatly", as.character(theme())))
+
+    expect_false(grepl("flatly", as.character(theme(theme = "darkly"))))
+
+    expect_true(grepl("`bootstrap-version` = \"3\"", as.character(theme())))
+    expect_false(
+      grepl("`bootstrap-version` = \"3\"", as.character(theme(version = 5)))
+    )
+
+    # Check if rubbish theme name returns an error
+    expect_error(theme("qwerty"))
+})
