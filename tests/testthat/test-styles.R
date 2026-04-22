0167355080229a9d749b2a958e6ec0e9a339f0c9
Arnfinn Hykkerud Steindal <arnfinn.steindal@gmail.com>
2026-04-22 09:46:17 +0200

    Endret funksjonsnavn fra theme/title til rapTheme/regTitle (#441)
    Ble i konflikt med funksjonsnavn fra ggplot2 etc.
    
    Beholder gamle funksjoner som wrapper rundt nye frem til alle rapportek
    har erstattet gamle kall med nye.
    
    Closes #438

--- tests/testthat/test-styles.R
+++ tests/testthat/test-styles.R
@@ -1,7 +1,7 @@
 
 # Test for the title function
 test_that("title function returns a div with the correct structure", {
-    result <- title("Test Title")
+    result <- regTitle("Test Title")
 
     # Check if the result is a shiny tag
     expect_s3_class(result, "shiny.tag")
@@ -14,6 +14,37 @@ test_that("title function returns a div with the correct structure", {
 })
 
 # Test for the theme function
+test_that("rapTheme function returns a valid shinytheme object", {
+
+    # Check if the theme name is correctly set
+    expect_true(grepl("flatly", as.character(rapTheme())))
+
+    expect_false(grepl("flatly", as.character(rapTheme(theme = "darkly"))))
+
+    expect_true(grepl("`bootstrap-version` = \"3\"", as.character(rapTheme())))
+    expect_false(
+      grepl("`bootstrap-version` = \"3\"", as.character(rapTheme(version = 5)))
+    )
+
+    # Check if rubbish theme name returns an error
+    expect_error(rapTheme("qwerty"))
+})
+
+# KOPI AV DEN OVER. SLETT NÅR FUNKSJONEN ER FJERNET FRA PAKKEN
+test_that("title function returns a div with the correct structure", {
+    result <- title("Test Title")
+
+    # Check if the result is a shiny tag
+    expect_s3_class(result, "shiny.tag")
+
+    # Check if the div contains the title text
+    expect_true(grepl("Test Title", as.character(result)))
+
+    # Check if the div contains a svg element (the logo)
+    expect_true(grepl("</svg>", as.character(result)))
+})
+
+# KOPI AV DEN OVER. SLETT NÅR FUNKSJONEN ER FJERNET FRA PAKKEN
 test_that("theme function returns a valid shinytheme object", {
 
     # Check if the theme name is correctly set
