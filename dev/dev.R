a3e4b5d689c0c18ff457ad8daabe605bd993eecd
arnfinn <arnfinn.steindal@gmail.com>
2026-05-29 16:29:28 +0200

    litt mer dev

--- dev/dev.R
+++ dev/dev.R
@@ -1,8 +1,11 @@
-
 source("dev/renv.R")
+
+# Hvis man er inne i docker-compose-miljøet (ikke på HN-IKT-windows-maskin).
+Sys.setenv(MYSQL_HOST = "db")
+Sys.setenv(RUN_MYSQLDUMP = "true")
+
 devtools::test()
 
-Sys.setenv(MYSQL_HOST = "db")
 rapbase::exportApp(teamName = "nord", dbName = "raplog", logAsJson = FALSE)
 
 rapbase::statsApp()
